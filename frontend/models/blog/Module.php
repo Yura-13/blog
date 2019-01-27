55be72805a10a0f9b28dcefb03f9c9d5c8b28f3b
Tea <tea@tea.77>
2019-01-27 13:56:22 +0400

    adding files

---
+++ frontend/models/blog/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\models\blog;
+
+/**
+ * blog module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\models\blog\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
